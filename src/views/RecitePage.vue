994ab5e00c05671aa928d89e52fd43f9b8923494
root <root@LAPTOP-4M1SAKEG.localdomain>
2026-08-06 22:28:44 +0800

    update RecitePage

--- src/views/RecitePage.vue
+++ src/views/RecitePage.vue
@@ -2,6 +2,7 @@
 import { computed, ref, watch } from "vue"
 import ReciteDictation from "@/components/ReciteDictation.vue"
 
+import book_01_lesson_12 from '@/data/book_01/lesson_text_12.json'
 import book_01_lesson_11 from '@/data/book_01/lesson_text_11.json'
 import book_01_lesson_10 from '@/data/book_01/lesson_text_10.json'
 import book_01_lesson_09 from '@/data/book_01/lesson_text_09.json'
@@ -15,6 +16,11 @@ const booksData = ref([
         volumeId: "v1",
         volumeName: "第 1 册",
         lessons: [
+          {
+            lessonId: "12",
+            lessonName: "第 12 课",
+            content: book_01_lesson_12
+          },
           {
             lessonId: "11",
             lessonName: "第 11 课：La première classes",
